4d47e271dc0f814de7f4b1e8100ad817d70e6f1e
BanzaiTokyo <banzaitokyo@gmail.com>
2024-04-13 14:27:08 +0200

    minor visual adjustments (logo, favicon, some text)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useCallback, useRef, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import cn from "classnames";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
+import EthIcon from "~~/components/svg/EthIcon";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
 type HeaderMenuLink = {
@@ -92,7 +92,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <EthIcon className="inline-block w-10 h-10 stroke-current" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Encode Solidity Bootcamp</span>
