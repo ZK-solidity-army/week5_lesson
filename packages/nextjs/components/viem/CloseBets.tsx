8a96a2e710dae1f95181a95f60f4d924f73aadf7
kobadmitrii <kobadmitrii@gmail.com>
2024-04-14 15:57:28 +0300

    Firs variant of Close Bets functions

--- packages/nextjs/components/viem/CloseBets.tsx
+++ packages/nextjs/components/viem/CloseBets.tsx
@@ -1,17 +1,45 @@
-import { useCallback } from "react";
+import { useCallback, useContext, useState } from "react";
+import TransactionList from "./TransactionList";
+import { useContractWrite } from "wagmi";
+import * as chains from "wagmi/chains";
+import ErrorBlock from "~~/components/Error";
+import { ContractContext } from "~~/context";
+import deployedContracts from "~~/contracts/deployedContracts";
 
 export default function CloseBets({ className }: { className?: string }) {
-  const onClick = useCallback(async () => {
-    console.log("Close bets");
-  }, []);
+  const [amount, setAmount] = useState<string>("");
+  const [txHashes, setTxHashes] = useState<string[]>([]);
+
+  const contractContext = useContext(ContractContext);
+
+  const { write, error } = useContractWrite({
+    address: contractContext.lotteryAddress,
+    abi: deployedContracts[chains.sepolia.id].Lottery.abi,
+    functionName: "closeLottery",
+    onSuccess: useCallback(
+      (data: { hash: string }) => {
+        setTxHashes(prev => [data.hash, ...prev]);
+      },
+      [setTxHashes],
+    ),
+  });
+
+  const onSubmit = useCallback(() => {
+    write({
+      args: [],
+    });
+  }, [amount, write]);
 
   return (
     <div className={className}>
-      {/* TODO: hardcoded alignment */}
-      <div className="mt-[84px]">
-        <button className="btn btn-neutral w-full" onClick={onClick}>
-          Close Bets
-        </button>
+      <div>
+        <div className="mt-2">
+          <button className="btn btn-neutral w-full" onClick={onSubmit}>
+            Close bets
+          </button>
+        </div>
+        <ErrorBlock className="text-center mt-5" error={error} />
+        <TransactionList className="mt-8" txHashes={txHashes} />
       </div>
     </div>
   );
