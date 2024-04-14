aa081691e54565a7bc2a2708cc01909d16059027
Guro Bokum <jiojiajiu@gmail.com>
2024-04-14 18:24:54 +0300

    feat: add additional information about smartcontract

---
+++ packages/nextjs/components/base/InfoRow.tsx
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+
+const dotClassName = twMerge(
+  "relative",
+  "before:content-[''] before:absolute before:w-full before:h-0 before:border-b-[0.125rem] before:border-dotted",
+  "before:leading-0 before:bottom-1 before:border-base-content before:z-0",
+  "flex flex-row justify-between items-center my-2",
+);
+const dotItemClassName = "bg-base-200 relative px-2";
+
+export default function InfoRow({ title, children }: { title: string; children: React.ReactNode }) {
+  return (
+    <div className={dotClassName}>
+      <div className={dotItemClassName}>{title}</div>
+      <div className={dotItemClassName}>{children}</div>
+    </div>
+  );
+}
--- packages/nextjs/context.ts
+++ packages/nextjs/context.ts
@@ -4,12 +4,14 @@ export type ContractContextType = {
   contractAddress?: `0x${string}`;
   lotteryAddress?: `0x${string}`;
   tokenAddress?: `0x${string}`;
+  ownerAddress?: `0x${string}`;
   tokenSymbol?: string;
   tokenDecimals?: number;
   purchaseRatio?: bigint;
   betPrice?: bigint;
   betFee?: bigint;
   tokenName?: string;
+  betsOpen?: boolean;
   isLoading?: boolean;
   isError?: boolean;
   setContractContext?: (contractContext: ContractContextType) => void;
