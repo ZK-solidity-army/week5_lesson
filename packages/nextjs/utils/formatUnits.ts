c2af6d62ac46a530a33f33cdeb304b5010d3e336
Guro Bokum <jiojiajiu@gmail.com>
2024-04-15 18:38:29 +0300

    fix: implement proper rounding in formatUnits

--- packages/nextjs/utils/formatUnits.ts
+++ packages/nextjs/utils/formatUnits.ts
@@ -7,13 +7,12 @@ export type FormatUnitsErrorType = ErrorType;
  * https://github.com/wevm/viem/blob/d2f93e726df1ab1ff86098d68a4406f6fae315b8/src/utils/unit/formatUnits.ts
  *
  * Allow to limit amount of zeros after the decimal point.
- * !Doesn't implement proper rounding.
  *
  * @example
  * limit = 3
  * formatUnits(1000000000000000000n, 18) // '1.0'
  * formatUnits(1000000100000000000n, 18) // '1.0000001'
- * formatUnits(1001713200000000000n, 18) // '1.001' - no proper rounding
+ * formatUnits(1001713200000000000n, 18) // '1.002'
  */
 export default function formatUnits(value: bigint, decimals: number, limit = 4) {
   let display = value.toString();
@@ -35,5 +34,22 @@ export default function formatUnits(value: bigint, decimals: number, limit = 4) 
   }
 
   limit = Math.max(limit, firstNonZero + 1);
+  // rounding
+  let carry = 0;
+  if (fraction.length >= limit && Number(fraction[limit]) >= 5) {
+    const a = fraction.split("");
+    carry = 1;
+    for (let i = limit - 1; i >= 0; i--) {
+      const v = Number(a[i]) + carry;
+      a[i] = (v % 10).toString();
+      if (v < 10) break;
+      limit -= 1;
+    }
+    fraction = a.slice(0, limit).join("");
+  }
+
+  if (carry) {
+    integer = (BigInt(integer) + 1n).toString();
+  }
   return `${negative ? "-" : ""}${integer || "0"}${fraction ? `.${fraction.slice(0, limit)}` : ""}`;
 }
